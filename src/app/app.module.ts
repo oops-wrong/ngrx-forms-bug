ab79155e80a14aaaaa578db1867f2f24a94d3d5c
Denis Kutalo <denis@kutalo.com>
2018-05-04 19:34:13 +0300

    Change material version

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,12 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { EffectsModule } from '@ngrx/effects';
 import { StoreModule } from '@ngrx/store';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
 import { AppComponent } from './app.component';
-import { metaReducers, reducers } from './app.reducer';
+import { metaReducers, reducers } from './reducers';
 import { environment } from '../environments/environment';
 import { SharedModule } from './shared/shared.module';
 
@@ -13,6 +14,7 @@ import { SharedModule } from './shared/shared.module';
   declarations: [AppComponent],
   imports: [
     BrowserModule,
+    BrowserAnimationsModule,
     SharedModule,
     StoreModule.forRoot(reducers, { metaReducers }),
     EffectsModule.forRoot([]),

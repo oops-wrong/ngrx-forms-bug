75bf24922a618797e898525537083d00bed861f1
Denis Kutalo <denis@kutalo.com>
2018-05-04 21:37:38 +0300

    Remove actions and replace form reducer

--- src/app/form.reducer.ts
+++ src/app/form.reducer.ts
@@ -1,6 +1,5 @@
+import { Action } from '@ngrx/store';
 import { createFormGroupReducerWithUpdate, createFormGroupState, FormGroupState } from 'ngrx-forms';
-
-import { FormActions, FormActionTypes } from './form.actions';
 
 export interface FormValue {
   select: string;
@@ -14,16 +13,13 @@ export interface State extends FormGroupState<FormValue> {}
 
 const formReducer = createFormGroupReducerWithUpdate<FormValue>({});
 
-export function reducer(state = initialFormState, action: FormActions): State {
+export function reducer(state = initialFormState, action: Action): State {
   const form = formReducer(state, action);
   if (form !== state) {
     state = { ...state, ...form };
   }
 
   switch (action.type) {
-    case FormActionTypes.FormAction:
-      return state;
-
     default:
       return state;
   }
--- src/app/reducers.ts
+++ src/app/reducers.ts
@@ -2,7 +2,7 @@ import { ActionReducerMap, MetaReducer } from '@ngrx/store';
 import { storeFreeze } from 'ngrx-store-freeze';
 
 import { environment } from '../environments/environment';
-import * as fromForm from './form/form.reducer';
+import * as fromForm from './form.reducer';
 
 export interface AppState {
   form: fromForm.State;
